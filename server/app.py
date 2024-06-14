64157a75ec5ebc46daafe1f3daa49a3f4a9aaf4b
Kevin Choi <kevinnwjchoi@gmail.com>
2024-06-13 20:26:29 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -1,3 +1,5 @@
+
+
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -14,13 +16,95 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.all():
+            message_dict = {
+                "id": message.id,
+                "body": message.body,
+                "username": message.username,
+                "created_at": message.created_at,
+                "updated_at": message.updated_at
+            }
+            messages.append(message_dict)
+
+        response = make_response(
+            messages,
+            200
+        )
+        return response
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data['body'],
+            username=data['username'],
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = {
+        "id": new_message.id,
+        "body": new_message.body,
+        "username": new_message.username,
+        "created_at": new_message.created_at,
+        "updated_at": new_message.updated_at
+    }
+
+        response = make_response(
+            message_dict,
+            201
+        )
+
+        return response
+
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if request.method == 'GET':
+        message_dict = message.to_dict()
+
+        response = make_response(
+            message_dict,
+            200
+        )
+        return response
+
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(message, attr, value)
+
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response = make_response(
+            message_dict,
+            200
+        )
+
+        return response
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.commit()
+
+        message_dict=message.to_dict()
+
+        response = make_response(
+            message_dict,
+            200
+        )
+        return response
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
---
+++ server/migrations/versions/3a88094da462_inital_revision.py
@@ -0,0 +1,24 @@
+"""inital revision
+
+Revision ID: 3a88094da462
+Revises: 
+Create Date: 2024-06-13 19:21:59.335066
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3a88094da462'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
